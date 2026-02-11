d6a01c0ba6f91f28d9c9c9b348baf9fbd45604cd
Faizan <2k23.cs2314006@gmail.com>
2026-02-11 21:59:58 +0530

    At the beginning starting with a Monolithic application

---
+++ src/main/java/com/example/jobApplication/job/impl/JobServiceImpl.java
@@ -0,0 +1,56 @@
+package com.example.jobApplication.job.impl;
+
+import com.example.jobApplication.job.Job;
+import com.example.jobApplication.job.JobRepository;
+import com.example.jobApplication.job.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class JobServiceImpl implements JobService {
+    @Autowired
+    JobRepository jobRepository;
+    @Override
+    public List<Job> findAll() {
+        return jobRepository.findAll();
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobRepository.save(job);
+    }
+
+    @Override
+    public boolean remove(Long id) {
+        if(findById(id)==null)
+            return false;
+        jobRepository.deleteById(id);
+        return true;
+    }
+
+    @Override
+    public Job findById(Long id) {
+        Optional<Job> job=jobRepository.findById(id);
+        return job.orElse(null);
+    }
+
+    @Override
+    public Job updateById(Long id, Job updatedJob) {
+        Job currentJob=findById(id);
+        if(currentJob!=null){
+            currentJob.setDescription(updatedJob.getDescription());
+            currentJob.setLocation(updatedJob.getLocation());
+            currentJob.setTitle(updatedJob.getTitle());
+            currentJob.setMinSalary(updatedJob.getMinSalary());
+            currentJob.setMaxSalary(updatedJob.getMaxSalary());
+            jobRepository.save(currentJob);
+            return currentJob;
+        }
+        return null;
+    }
+}
