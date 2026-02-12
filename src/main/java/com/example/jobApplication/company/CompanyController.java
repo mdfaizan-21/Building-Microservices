ad3a51ddad4f20ec44f63a0839c8314f708c1f1d
Faizan <2k23.cs2314006@gmail.com>
2026-02-12 22:29:34 +0530

    Building Job Application Review System

---
+++ src/main/java/com/example/jobApplication/company/CompanyController.java
@@ -0,0 +1,50 @@
+package com.example.jobApplication.company;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+    @Autowired
+    CompanyService companyService;
+    @GetMapping
+    public ResponseEntity<List<Company>> getAllCompany(){
+        return ResponseEntity.ok(companyService.getAllCompanies());
+    }
+    @GetMapping("/{id}")
+    public  ResponseEntity<Company> getCompany(@PathVariable Long id){
+        Company company=companyService.getCompanyById(id);
+        if(company!=null){
+            return  ResponseEntity.ok(company);
+        }
+        return  ResponseEntity.status(404).body(null);
+    }
+
+    @PostMapping
+    public ResponseEntity<Company> addCompany(@RequestBody Company newCompany){
+        companyService.addCompany(newCompany);
+        return ResponseEntity.ok(newCompany);
+    }
+
+    @PutMapping("/{id}")
+    public  ResponseEntity<Company> updateCompany(@RequestBody Company company,@PathVariable Long id){
+        if(companyService.updateCompanyById(id,company)!=null){
+            return  ResponseEntity.ok(company);
+        }
+        return  ResponseEntity.status(404).body(null);
+    }
+
+
+    @DeleteMapping
+    public  ResponseEntity<String> deleteCompany(@PathVariable Long id){
+        if(companyService.remove(id)){
+            return ResponseEntity.ok("Company Removed Successfully");
+        }
+        return  ResponseEntity.status(404).body("Company with this company id does not exist");
+    }
+}
