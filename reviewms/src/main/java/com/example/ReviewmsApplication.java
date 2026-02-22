d40b47953481ba07834b2b11cb47e2b089f4fba8
Faizan <2k23.cs2314006@gmail.com>
2026-02-22 22:46:33 +0530

    connect all microservice to postgreSQL database

--- reviewms/src/main/java/com/example/ReviewmsApplication.java
+++ reviewms/src/main/java/com/example/ReviewmsApplication.java
@@ -3,11 +3,14 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ReviewmsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReviewmsApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(ReviewmsApplication.class, args);
 	}
 
 }
