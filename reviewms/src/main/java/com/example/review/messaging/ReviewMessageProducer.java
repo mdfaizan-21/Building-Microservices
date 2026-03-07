10ba49b47b198955bc25130c7347c663e9bc5662
Faizan <2k23.cs2314006@gmail.com>
2026-03-07 16:16:50 +0530

    added producer with RabbitMQ to send messages

---
+++ reviewms/src/main/java/com/example/review/messaging/ReviewMessageProducer.java
@@ -0,0 +1,25 @@
+package com.example.review.messaging;
+
+import com.example.dto.ReviewMessage;
+import com.example.review.Review;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewMessageProducer {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(Review review){
+        ReviewMessage reviewMessage = new ReviewMessage();
+        reviewMessage.setId(review.getId());
+        reviewMessage.setTitle(review.getTitle());
+        reviewMessage.setDescription(review.getDescription());
+        reviewMessage.setRating(review.getRating());
+        reviewMessage.setCompanyId(review.getCompanyId());
+        rabbitTemplate.convertAndSend("companyRatingQueue", reviewMessage);
+        System.out.println("Review message sent: " + reviewMessage.toString());
+    }
+}
